65076f0d039bccc9daaa7e7648c08406fb5e0c04
anthonyNgan <anthonyngan74@gmail.com>
2020-09-19 01:41:39 +1200

    'social_media_sharing'

--- View/app/src/App.tsx
+++ View/app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import clsx from 'clsx';
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import clsx from 'clsx';
+
 import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
@@ -10,10 +11,10 @@ import Paper from '@material-ui/core/Paper';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 
 import AppMenu from '../src/components/AppMenu/AppMenu';
-
 import Dashboard from '../src/components/Dashboard/Dashboard';
 import Chiropractors from '../src/components/Chiropractors/Chiropractors';
 import Patients from '../src/components/Patients/Patients';
+import SocialMedia from '../src/components/Share/SocialMedia';
 
 const PageDashboard = () => <Dashboard />
 const PageChiropractors = () => <Chiropractors />
@@ -36,14 +37,10 @@ const App: React.FC = () => {
                 <BrowserRouter>
                     <div className={clsx('App', classes.root)}>
                         <CssBaseline />
-                        <Drawer
-                            variant="permanent"
-                            classes={{
-                                paper: classes.drawerPaper,
-                            }}
-                        >
+                        <Drawer variant="permanent" classes={{paper: classes.drawerPaper,}}>
                             <AppMenu />
                             <ToggleButton selected={darkMode} onChange={() => setDarkMode(!darkMode)}>DarkMode</ToggleButton>
+                            <SocialMedia />
                         </Drawer>
                         <main className={classes.content}>
                             <Container maxWidth="lg" className={classes.container}>
@@ -54,6 +51,7 @@ const App: React.FC = () => {
                                     <Route path="/patients" component={PagePatients} />
                                 </Switch>
                             </Container>
+                            
                         </main>
                     </div>
                 </BrowserRouter>
@@ -85,6 +83,7 @@ const useStyles = makeStyles(theme => ({
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4),
     },
+   
 }));
 
 
